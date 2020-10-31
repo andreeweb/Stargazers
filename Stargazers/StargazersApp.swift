0da4360265d8b7752877e58cbc07e158bc83e2b0
Andrea <andrea.cerra@me.com>
2020-10-31 17:24:44 +0100

    Project setup

---
+++ Stargazers/StargazersApp.swift
@@ -0,0 +1,17 @@
+//
+//  StargazersApp.swift
+//  Stargazers
+//
+//  Created by MacOS on 10/31/20.
+//
+
+import SwiftUI
+
+@main
+struct StargazersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

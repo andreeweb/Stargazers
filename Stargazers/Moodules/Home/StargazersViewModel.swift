edbc215c819da3211b0cf8a2df01376284365dba
Andrea <andrea.cerra@me.com>
2020-11-01 22:31:01 +0100

    Added avatar download logic

--- Stargazers/Moodules/Home/StargazersViewModel.swift
+++ Stargazers/Moodules/Home/StargazersViewModel.swift
@@ -14,6 +14,7 @@ final class StargazersViewModel: ObservableObject {
     @Published private(set) var stargazers: [Stargazer] = []
     
     private var subscription: Cancellable? = nil
+    private var imageSubscriptions: [Cancellable] = []
     
     private let githubService: GitHubServiceProtocol
     private let imageService: ImageServiceProtocol
@@ -43,13 +44,56 @@ final class StargazersViewModel: ObservableObject {
     
     private func parseData(data: [StargazerData]){
         
-        for stargazer in data {
+        for stargazerData in data {
             
-            let stargazer = Stargazer(name: stargazer.login,
+            let stargazer = Stargazer(name: stargazerData.login,
                                       image: #imageLiteral(resourceName: "avatar-placeholder"),
                                       isLoadingImage: false)
             
             stargazers.append(stargazer)
+            
+            // async download image
+            downloadImage(for: stargazerData)
+        }
+    }
+    
+    private func downloadImage(for stargazer: StargazerData){
+        
+        let sub = imageService.getImageFromUrl(url: stargazer.avatar_url).sink(
+            receiveCompletion: { [weak self] completion in
+                
+                switch completion {
+                case .finished:
+                    break
+                case .failure:
+                    self?.updateStargazerWithImage(image: nil, stargazer: stargazer)
+                }
+                
+            }, receiveValue: { [weak self] (image) in
+                
+                self?.updateStargazerWithImage(image: image, stargazer: stargazer)
+        })
+        
+        imageSubscriptions.append(sub)
+    }
+    
+    private func updateStargazerWithImage(image: UIImage?,
+                                          stargazer: StargazerData) {
+        
+        var backgroundImage = #imageLiteral(resourceName: "avatar-placeholder")
+        
+        if image != nil {
+            backgroundImage = image!
+        }
+        
+        let stargazer = Stargazer(name: stargazer.login,
+                                  image: backgroundImage,
+                                  isLoadingImage: false)
+        
+        stargazers.append(stargazer)
+        
+        if let i = stargazers.firstIndex(where: { $0.name == stargazer.name }) {
+            stargazers[i] = stargazer
         }
     }
     
